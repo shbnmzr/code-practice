1e98ce65d81833d325023d87d2ef3ca339738023
Shabnam Zareshahraki <shabnam.zareshahraki@consat.se>
2023-07-21 15:31:08 +0200

    Session management added

--- Django/personal_site/blog/urls.py
+++ Django/personal_site/blog/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('', views.StaringPageView.as_view(), name='starting_page'),
     path('posts', views.Posts.as_view(), name='posts_page'),
     path('posts/<slug:slug>', views.PostDetails.as_view(), name='post_details_page'),
+    path('read_later', views.ReadLaterView.as_view(), name='read_later'),
 ]
--- Django/personal_site/blog/views.py
+++ Django/personal_site/blog/views.py
@@ -55,3 +55,33 @@ class PostDetails(View):
             'comment_form': CommentForm(),
         }
         return render(request, 'blog/post_details.html', context)
+
+
+class ReadLaterView(View):
+    def get(self, request):
+        stored_posts = request.session.get('stored_posts')
+        context = {}
+        if stored_posts is None or len(stored_posts) == 0:
+           context['posts'] = []
+           context['has_posts'] = False
+
+        else:
+            posts = Post.objects.filter(id__in=stored_posts)
+            context['posts'] = posts
+            context['has_posts'] = True
+
+        return render(request, 'blog/stored_posts.html', context)
+
+
+
+    def post(self, request):
+        stored_posts = request.session.get('stored_posts')
+
+        if stored_posts is None:
+            stored_posts = []
+        
+        if (id:=int(request.POST['post_id'])) not in stored_posts:
+            stored_posts.append(id)
+            request.session['stored_posts'] = stored_posts
+
+        return HttpResponseRedirect('/')
